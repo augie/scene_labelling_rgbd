3c8775224db86941d80241c8e6c91cfb814ba378
Augie Hill <augie@umich.edu>
2012-02-18 12:21:11 -0500

    Updated the top level README and install dependcies shell script.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,7 @@
 set -e
 wget http://ftp.gnu.org/gnu/glpk/glpk-4.45.tar.gz
 tar -xvzf glpk-4.45.tar.gz
+rm glpk-4.45.tar.gz
 cd glpk-4.45/
 ./configure
 make
@@ -9,6 +10,7 @@ cd ..
 
 wget ftp://ftp.gmplib.org/pub/gmp-5.0.2/gmp-5.0.2.tar.bz2
 tar -xjvf gmp-5.0.2.tar.bz2
+rm gmp-5.0.2.tar.bz2
 cd gmp-5.0.2
 ./configure
 make
@@ -17,6 +19,7 @@ cd ..
 
 wget http://tfinley.net/software/pyglpk/pyglpk-0.3.tar.bz2
 tar -xjvf pyglpk-0.3.tar.bz2
+rm pyglpk-0.3.tar.bz2
 # change -m32 to -m64 in setup.py
 # comment the following line if running on a 32 bit machine
 cp pyglpk_setup.py pyglpk-0.3/setup.py
@@ -27,6 +30,7 @@ cd ..
 
 wget http://pypi.python.org/packages/source/b/bitarray/bitarray-0.3.5.tar.gz#md5=322a25e04e7aece5e028e5b068329291
 tar -xzvf bitarray-0.3.5.tar.gz
+rm bitarray-0.3.5.tar.gz
 cd bitarray-0.3.5
 sudo python setup.py install
 cd ..
@@ -38,5 +42,7 @@ cd ..
 
 sudo apt-get install python-numpy python-scipy
 
-
-export PYTHONPATH=$PYTHONPATH:/home/nav/pkg/scene_labelling/svm-python-v204/pyobjs/build/lib.linux-x86_64-2.6/
+echo
+echo
+echo "Now run the following command, modifying [SCENE_LABELLING_DIR] appropriately: "
+echo " export PYTHONPATH=$PYTHONPATH:[SCENE_LABELLING_DIR]/svm-python-v204/pyobjs/build/lib.linux-x86_64-2.6/"
