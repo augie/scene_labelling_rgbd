c244255ecd370c0c60c0e005d00f8140599e96ba
Augie Hill <augie@umich.edu>
2012-02-18 16:18:40 -0500

    Further cleaning up scripts to run scripts from a working directory.

--- svm-python-v204/run_sequential.sh
+++ svm-python-v204/run_sequential.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
+
 data=$1
-descrip="$1 data (other info can go here)"
-echo $descrip
-#method=sum1.IP
 loss=micro
 lmethod=objassoc
 cmethod=sum1.IP
-objmapfile="../"$data"_objectMap.txt"
+objmapfile="objectmap.txt"
 c=0.001
 e=0.01
+
 for i in `seq 1 4`
 do
+  suffix=c$c.e$e.$lmethod
+  modelFile=model.$suffix
 
-suffix=c$c.e$e.$lmethod
-modelFile=model.$suffix
+  modelFolder=data/$data/fold$i/models
+  echo "out.$method.$modelFile" >> data/$data/fold$i/lastout.txt
+  sh run_svm.sh $c $e $i $modelFile $modelFolder $suffix $cmethod $lmethod $loss $objmapfile $data 
+done
 
-modelFolder=data/$data/fold$i/models
-echo "out.$method.$modelFile" >> data/$data/fold$i/lastout.txt
-sh run_svm.sh $c $e $i $modelFile $modelFolder $suffix $cmethod $lmethod $loss $objmapfile $data 
-done 
-  
 echo "processes completed!"
 perl get_avg_pr.pl out.$cmethod.$modelFile data/$data/ > data/$data/avg_pr.$cmethod.$modelFile
 method=$suffix.$cmethod
@@ -29,7 +27,6 @@ rm runinfo
 echo $HOSTNAME >> runinfo
 pwd >> runinfo
 
-echo "description: $descrip" >> runinfo
 echo "method : $method" >> runinfo
 echo "loss: $loss" >> runinfo
 
@@ -47,4 +44,3 @@ echo "~~~~~~~~~~~~~~~" >> runinfo
 echo "" >> runinfo
 echo "" >> runinfo
 cat data/$data/confusionM.$method >> runinfo
-
