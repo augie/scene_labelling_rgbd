4da548f6e7265dd37c41900d19c88b6504294d17
Augie Hill <augie@umich.edu>
2012-03-08 17:55:50 -0500

    Updated initialize

--- svm-python-v204/initialize.sh
+++ svm-python-v204/initialize.sh
@@ -1,26 +1,31 @@
 WORKDIR=$1
-rm $WORKDIR/data_nodefeats.*.txt $WORKDIR/data_edgefeats.*.txt $WORKDIR/datas_*.txt > /dev/null 2> /dev/null
-cp $WORKDIR/data_nodefeats.txt ./data_nodefeats.txt
-cp $WORKDIR/data_edgefeats.txt ./data_edgefeats.txt
+cp $WORKDIR/data_nodefeats.txt ./
+cp $WORKDIR/data_edgefeats.txt ./
+cp $WORKDIR/labelmap.txt ./
 perl filter.pl data_nodefeats.txt
 perl filter.pl data_edgefeats.txt
 matlab -nodesktop -nosplash -r binfeats
-cat header_data_nodefeats.txt temp_data_nodefeats.b.txt > $WORKDIR/data_nodefeats.b.txt
-cat header_data_edgefeats.txt temp_data_edgefeats.b.txt > $WORKDIR/data_edgefeats.b.txt
-#rm temp_data_* header_data_* > /dev/null 2> /dev/null
-#perl ../../format.pl data_nodefeats.b.txt data_edgefeats.b.txt labelmap.txt 1
+cat header_data_nodefeats.txt temp_data_nodefeats.b.txt > data_nodefeats.b.txt
+cat header_data_edgefeats.txt temp_data_edgefeats.b.txt > data_edgefeats.b.txt
+rm temp_data_* header_data_* > /dev/null 2> /dev/null
+perl format.pl data_nodefeats.b.txt data_edgefeats.b.txt labelmap.txt 1
+mv ./data_nodefeats.b.txt $WORKDIR/
+mv ./data_edgefeats.b.txt $WORKDIR/
+mv ./datas_* $WORKDIR/
+rm data_nodefeats.*.txt data_edgefeats.*.txt datas_*.txt > /dev/null 2> /dev/null
 
-#for i in `seq 1 4`
-#do
-#  mkdir fold$i > /dev/null 2> /dev/null
-#  mkdir fold$i/pred/ > /dev/null 2> /dev/null
-#  mkdir fold$i/logs/ > /dev/null 2> /dev/null
-#  mkdir fold$i/models/ > /dev/null 2> /dev/null
-#  mkdir fold$i/imodels/ > /dev/null 2> /dev/null
-#  touch fold$i/test$i
-#  touch fold$i/train$i
-#  rm fold$i/pred/* >/dev/null 2> /dev/null
-#  rm fold$i/logs/* > /dev/null 2> /dev/null
-#  rm fold$i/models/* > /dev/null 2> /dev/null
-#  rm fold$i/imodels/* > /dev/null 2> /dev/null
-#done
+cd $WORKDIR
+for i in `seq 1 4`
+do
+  mkdir fold$i > /dev/null 2> /dev/null
+  mkdir fold$i/pred/ > /dev/null 2> /dev/null
+  mkdir fold$i/logs/ > /dev/null 2> /dev/null
+  mkdir fold$i/models/ > /dev/null 2> /dev/null
+  mkdir fold$i/imodels/ > /dev/null 2> /dev/null
+  touch fold$i/test$i
+  touch fold$i/train$i
+  rm fold$i/pred/* >/dev/null 2> /dev/null
+  rm fold$i/logs/* > /dev/null 2> /dev/null
+  rm fold$i/models/* > /dev/null 2> /dev/null
+  rm fold$i/imodels/* > /dev/null 2> /dev/null
+done
